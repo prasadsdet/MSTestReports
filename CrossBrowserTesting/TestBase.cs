e764c3505e88d4da26cf5cbe7a505ab2335f895e
prasad.nunna@opsi.co.za <prasad.nunna@opsi.co.za>
2020-06-26 16:47:07 +0200

    Initial Commit[MSTest Extent Reports]

---
+++ CrossBrowserTesting/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossBrowserTesting.Properties
+{
+    public class TestBase 
+    {
+
+        public static ExtentReports extent;
+        public static ExtentHtmlReporter htmlReportr;       
+        public static ExtentTest parentTest;
+        public static ExtentTest childTest;
+        public static string dirpath;
+
+        public  static void reportLogger(string testCaseName)
+        {
+            extent = new ExtentReports();
+            var dir = AppDomain.CurrentDomain.BaseDirectory.Replace("\\bin\\Debug", "");
+
+            Directory.CreateDirectory(dir + "\\TetsExecutionReports\\");
+            Random rand = new Random();
+            string randdomno = rand.Next(1000).ToString();
+            dirpath = dir + "\\TestExecutionReports\\Test_Execution_Reports" + "_" + testCaseName;
+            htmlReportr = new ExtentHtmlReporter(dirpath);
+            htmlReportr.Config.Theme = Theme.Dark;
+            extent.AttachReporter(htmlReportr);
+            extent.AddSystemInfo("Host Name", System.Net.Dns.GetHostName());
+            extent.AddSystemInfo("User Name", "Prasad.Nunna");
+
+        }
+    }
+}
---
+++ CrossBrowserTesting/TestSuite.cs
@@ -0,0 +1,62 @@
+﻿using AventStack.ExtentReports;
+using CrossBrowserTesting.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossBrowserTesting
+{
+    [TestClass]
+    public class TestSuite : TestBase
+    {
+        public TestContext TestContext { get; set; }
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext testContext)
+        {
+            reportLogger("UITEsts");
+        }
+
+        [TestInitialize]
+        public void init()
+        {
+            Console.WriteLine(TestContext.TestName);
+        }
+
+
+        [TestMethod,TestCategory("ExtentTest")]
+        public void TC1extentTestPASS()
+        {
+           // reportLogger(TestContext.TestName);
+            parentTest = extent.CreateTest(TestContext.TestName);
+            childTest = parentTest.CreateNode("Provide Parameter");
+            childTest.Log(Status.Pass, "Test Passed1");
+            int a = 100;
+            int b = 200;
+            int c = a + b;
+
+        }
+
+        [TestMethod, TestCategory("ExtentTest")]
+        public void TC2extentTestFAIL()
+        {
+           // reportLogger(TestContext.TestName);
+            parentTest = extent.CreateTest(TestContext.TestName);
+            childTest = parentTest.CreateNode("Provide Parameter1");
+            childTest.Log(Status.Fail, "Test Failed");
+            int a = 100;
+            int b = 200;
+            int c = a + b;
+        }
+
+
+        [AssemblyCleanup]
+        public static void Testclean()
+        {
+            extent.Flush();
+        }
+    }
+}
